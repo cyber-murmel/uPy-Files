491e287c4119a98081ad23c6fd4e3ba1a2f7756c
marble <marble@chaoscomputer.club>
2017-08-31 16:25:26 +0200

    changed script to be able to handle a list of possible aps to connect to

--- esp32_esp8266/esp32/boot.py
+++ esp32_esp8266/esp32/boot.py
@@ -1,33 +1,70 @@
 # This file is executed on every boot (including wake-boot from deepsleep
 
 from machine import reset
-from machine import Pin
-from machine import TouchPad
-import network
+from network import WLAN, STA_IF, phy_mode, MODE_11N
 from time import ticks_ms
 from gc import collect as gc_collect
 import ujson as json
+from socket import socket as sock, AF_INET, SOCK_DGRAM, IPPROTO_UDP, getaddrinfo
 
-STA_CONF_PATH = "/sta_conf.json"
+STA_CONF_ARR_PATH = "/sta_conf_arr.json"
+PHY_MODE = MODE_11N
 STA_TIMEOUT = 5000
+AF_INET_BROADCAST = "255.255.255.255"
+ADDR_ANNOUNCE_PORT = 1337
 
-def connect_to_wifi():
-    sta_conf = json.loads(open(STA_CONF_PATH).read())
-    network.phy_mode(network.MODE_11G)
-    sta_if = network.WLAN(network.STA_IF)
+def scan_wifi():
+    sta_if = WLAN(STA_IF)
     sta_if.active(True)
-    sta_if.connect(sta_conf["ssid"], sta_conf["pass"])
+    scan = sta_if.scan()
+    print("\nWiFi Scan:\n\t{:35}, {:14}, {:2}, {:4}, {:4}, {:6}".format("ssid", "bssid", "ch", "rssi", "auth", "hidden"))
+    for ssid, bssid, channel, rssi, authmode, hidden in scan:
+        print("\t{:35}, 0x{:12}, {:2}, {:4}, {:4}, {:6}".format(ssid, "".join(["{:02x}".format(b) for b in bssid])[:-1], channel, rssi, authmode, hidden))
+    print("")
+    return scan
+
+def connect_to_ap(ssid, psk):
+    sta_if = WLAN(STA_IF)
+    sta_if.active(True)
+    sta_if.connect(ssid, psk)
     now = ticks_ms()
     while (not sta_if.isconnected()) and ((ticks_ms()-now < STA_TIMEOUT)):
         pass
     if not sta_if.isconnected():
         raise OSError("Could not connect to AP")
 
-if __name__ == "__main__":
+def deactivate_sta():
+    sta_if = WLAN(STA_IF)
+    sta_if.active(True)
+    sta_if.connect("", "")
+    sta_if.active(False)
+
+def connect_to_wifi():
+    sta_conf_arr = json.load(open(STA_CONF_ARR_PATH))
+    for ssid, _, _, _, _, _ in scan_wifi():
+        for sta_conf in sta_conf_arr:
+            if ssid.decode() == sta_conf["ssid"]:
+                print("Connecting to "+sta_conf["ssid"])
+                try:
+                    connect_to_ap(sta_conf["ssid"], sta_conf["psk"])
+                except OSError as e:
+                    pass
+                else:
+                    print("Connected to "+sta_conf["ssid"])
+                    return
+    deactivate_sta()
+    raise OSError("No access point available.")
+
+if __name__ == '__main__':
+    phy_mode(PHY_MODE)
     try:
         connect_to_wifi()
+        s = sock(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+        s.connect((AF_INET_BROADCAST, ADDR_ANNOUNCE_PORT))
+        s.send("{}\n".format(WLAN().ifconfig()[0]))
+        s.close()
     except KeyError as err:
         print("Key Error: {0}".format(err))
     except OSError as err:
         print("OS Error: {0}".format(err))
-    gc_collect()
+    gc.collect()
--- esp32_esp8266/esp8266/boot.py
+++ esp32_esp8266/esp8266/boot.py
@@ -1,28 +1,62 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 import gc
-import network
+from network import WLAN, STA_IF, phy_mode, MODE_11N, STAT_CONNECTING
+from network import STAT_GOT_IP
 import webrepl
+from machine import reset
+import ujson as json
 
-STA_CONF_PATH = "/sta_conf.json"
+STA_CONF_ARR_PATH = "/sta_conf_arr.json"
+PHY_MODE = MODE_11N
+
+def scan_wifi():
+    sta_if = WLAN(STA_IF)
+    sta_if.active(True)
+    scan = sta_if.scan()
+    print("\nWiFi Scan:\n\t{:35}, {:14}, {:2}, {:4}, {:4}, {:6}".format("ssid", "bssid", "ch", "rssi", "auth", "hidden"))
+    for ssid, bssid, channel, rssi, authmode, hidden in scan:
+        print("\t{:35}, 0x{:12}, {:2}, {:4}, {:4}, {:6}".format(ssid, "".join(["{:02x}".format(b) for b in bssid])[:-1], channel, rssi, authmode, hidden))
+    print("")
+    return scan
+
+def connect_to_ap(ssid, psk):
+    sta_if = WLAN(STA_IF)
+    sta_if.active(True)
+    sta_if.connect(ssid, psk)
+    while sta_if.status() == STAT_CONNECTING:
+        pass
+    if sta_if.status() != STAT_GOT_IP:
+        raise OSError("Could not connect.")
+
+def deactivate_sta():
+    sta_if = WLAN(STA_IF)
+    sta_if.active(True)
+    sta_if.connect("", "")
+    sta_if.active(False)
 
 def connect_to_wifi():
-    sta_conf = json.loads(open(STA_CONF_PATH).read())
-    network.phy_mode(network.MODE_11N)
-    sta_if = network.WLAN(network.STA_IF)
-    sta_if.active(True)
-    sta_if.connect(sta_conf["ssid"], sta_conf["pass"])
-    while network.STAT_CONNECTING == sta_if.status():
-        pass
-    if network.STAT_GOT_IP != sta_if.status():
-        print("Could not connect to WiFi")
-        raise OSError
+    sta_conf_arr = json.load(open(STA_CONF_ARR_PATH))
+    for ssid, _, _, _, _, _ in scan_wifi():
+        for sta_conf in sta_conf_arr:
+            if ssid.decode() == sta_conf["ssid"]:
+                print("Connecting to "+sta_conf["ssid"])
+                try:
+                    connect_to_ap(sta_conf["ssid"], sta_conf["psk"])
+                except OSError as e:
+                    pass
+                else:
+                    print("Connected to "+sta_conf["ssid"])
+                    return
+    deactivate_sta()
+    raise OSError("No access point available.")
 
 if __name__ == '__main__':
-    webrepl.start()
+    phy_mode(PHY_MODE)
     try:
         connect_to_wifi()
     except KeyError as err:
         print("Key Error: {0}".format(err))
     except OSError as err:
         print("OS Error: {0}".format(err))
+    webrepl.start()
     gc.collect()
